1fadfa9b8c06a0ca59060a58dcee857432b86e9e
Ebad Husain <ebad.husain95@gmail.com>
2025-12-28 23:05:52 +0530

    Added polling service to poll data and publish events

--- src/main/java/com/example/TransactionalOutboxPattern/Config/AppConfig.java
+++ src/main/java/com/example/TransactionalOutboxPattern/Config/AppConfig.java
@@ -1,8 +1,15 @@
 package com.example.TransactionalOutboxPattern.Config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -17,5 +24,23 @@ public class AppConfig {
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
     }
-    
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(senderProps());
+    }
+
+    private Map<String, Object> senderProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.springframework.kafka.support.serializer.JsonSerializer");
+        return props;
+    }
+
+    @Bean
+    @Primary
+    public KafkaTemplate<String,Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<String, Object>(producerFactory);
+    }
 }
--- src/main/java/com/example/TransactionalOutboxPattern/Repository/OutboxRepository.java
+++ src/main/java/com/example/TransactionalOutboxPattern/Repository/OutboxRepository.java
@@ -1,10 +1,15 @@
 package com.example.TransactionalOutboxPattern.Repository;
 
 import com.example.TransactionalOutboxPattern.Entity.Outbox;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OutboxRepository extends JpaRepository<Outbox, Long> {
+
+    List<Outbox> findTop10ByIsDelivered(int status);
 
 }
